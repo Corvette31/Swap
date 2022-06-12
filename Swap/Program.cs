738a547789be8c9d45526333c6549e1bf9091dff
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 21:37:48 +0300

    Добавьте файлы проекта.

---
+++ Swap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lastName = "Иван";
+            string firstName = "Иванов";
+            string temp = lastName;
+            lastName = firstName;
+            firstName = temp;
+            Console.WriteLine($"Имя : {firstName} , Фамилия : {lastName}");
+        }
+    }
+}
